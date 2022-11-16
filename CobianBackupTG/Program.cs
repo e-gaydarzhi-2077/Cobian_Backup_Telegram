d4c1c881c54459054f3b2cea823c56f9e5ff992b
Eugene Gaydarzhi <107859162+e-gaydarzhi-2077@users.noreply.github.com>
2022-11-16 20:00:03 +0200

    fix2

--- CobianBackupTG/Program.cs
+++ CobianBackupTG/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CobianBackupTG
 {
@@ -13,21 +10,20 @@ namespace CobianBackupTG
         static void Main(string[] args)
         {
             //Vars
+            string DirExe = System.Reflection.Assembly.GetExecutingAssembly().Location;
             string DateNow = DateTime.Now.ToString("yyyy-MM-dd");
             string PathLog = "C:\\Program Files (x86)\\Cobian Backup 11\\Logs\\";
             string PathLogTemp = "C:\\Program Files (x86)\\Cobian Backup 11\\Logs\\temp\\";
-            string BotID = File.ReadLines(System.Environment.CurrentDirectory + "\\CobianBackupTG.cfg").ElementAtOrDefault(0);
-            string BotChatID = File.ReadLines(System.Environment.CurrentDirectory + "\\CobianBackupTG.cfg").ElementAtOrDefault(1);
+            string BotID = File.ReadLines(Path.GetDirectoryName(DirExe) + "\\CobianBackupTG.cfg").ElementAtOrDefault(0);
+            string BotChatID = File.ReadLines(Path.GetDirectoryName(DirExe) + "\\CobianBackupTG.cfg").ElementAtOrDefault(1);
 
             //Create Temp if not exist
-            System.IO.Directory.CreateDirectory(PathLogTemp);
+            Directory.CreateDirectory(PathLogTemp);
 
             if (File.Exists(PathLog + "log " + DateNow + ".txt"))
             {
-                Console.WriteLine("File exists");
-
-                //Copy Opened File
-                System.IO.File.Copy(PathLog + "log " + DateNow + ".txt", PathLogTemp + "log " + DateNow + ".txt", true);
+                //Copy Opened File To Temp Folder
+                File.Copy(PathLog + "log " + DateNow + ".txt", PathLogTemp + "log " + DateNow + ".txt", true);
 
                 //Get Public IP
                 string externalIpString = new WebClient().DownloadString("http://icanhazip.com").Replace("\\r\\n", "").Replace("\\n", "").Trim();
@@ -36,7 +32,6 @@ namespace CobianBackupTG
                 //Read File
                 var lines = File.ReadLines(PathLogTemp + "log " + DateNow + ".txt");
                 string result = string.Join("\n", lines.Where(s => s.IndexOf("ERR", StringComparison.InvariantCultureIgnoreCase) >= 0));
-                Console.WriteLine(result);
 
                 try
                 {
@@ -47,13 +42,21 @@ namespace CobianBackupTG
                     }
                     else
                     {
-                        //Send result in Telegram
-                        string GetBot = "Ошибка Cobian Backup ⚠️ " + "\n" + "Пользователь: " + Environment.UserName + "\n" + "Компьютер: " + Environment.MachineName + "\n" + "IP-Адресс: " + externalIp.ToString() + "\n" + "`" + result + "`";
-                        System.Net.WebRequest reqGET = System.Net.WebRequest.Create(@"https://api.telegram.org/bot" + BotID + "/sendMessage?chat_id=" + BotChatID + "&parse_mode=Markdown&text=" + GetBot);
-                        System.Net.WebResponse resp = reqGET.GetResponse();
-                        System.IO.Stream stream = resp.GetResponseStream();
-                        System.IO.StreamReader sr = new System.IO.StreamReader(stream);
-                        Console.WriteLine("Ошибки найдены и отправлены");
+                        //Check results if have upper characters contains
+                        if (!result.Contains("ERR"))
+                        {
+                            Console.WriteLine("ERR: " + result.Contains("ERR") + "!");
+                        }
+                        else
+                        {
+                            //Send result in Telegram
+                            string GetBot = "Error Cobian Backup ⚠️ " + "\n" + "User: " + Environment.UserName + "\n" + "Computer: " + Environment.MachineName + "\n" + "IP-Adress: " + externalIp.ToString() + "\n" + "`" + result + "`";
+                            WebRequest reqGET = WebRequest.Create(@"https://api.telegram.org/bot" + BotID + "/sendMessage?chat_id=" + BotChatID + "&parse_mode=Markdown&text=" + GetBot);
+                            WebResponse resp = reqGET.GetResponse();
+                            Stream stream = resp.GetResponseStream();
+                            StreamReader sr = new StreamReader(stream);
+                            Console.WriteLine("Errors Finded");
+                        }
                     }
                 }
                 catch
@@ -63,7 +66,8 @@ namespace CobianBackupTG
                 finally
                 {
                     //Delete temp file and exit
-                    System.IO.File.Delete(PathLogTemp + "log " + DateNow + ".txt");
+                    File.Delete(PathLogTemp + "log " + DateNow + ".txt");
+                    Directory.GetFiles(PathLogTemp).ToList().ForEach(File.Delete);
                     Environment.Exit(0);
                 }
             }
@@ -72,7 +76,6 @@ namespace CobianBackupTG
                 Console.WriteLine("File not exists");
                 Environment.Exit(0);
             }
-            
         }
     }
 }
